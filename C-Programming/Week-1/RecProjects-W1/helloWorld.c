2328a2c06cbe5cb6d0d96e9757815b154a702ba5
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-03 00:35:25 -0400

    Update helloWorld.c

--- C-Programming/Week-1/RecProjects-W1/helloWorld.c
+++ C-Programming/Week-1/RecProjects-W1/helloWorld.c
@@ -6,8 +6,8 @@
 #include <stdio.h> // "stdio.h" is the standard C library for functions
 
 int main() {
-    printf("At least now I remember how to correctly complile my C code!\n");
+    printf("At least now I remember how to correctly compile my C code!\n");
     printf("I am not sure if I will be able to do it again, but at least I know how to do it now!\n");
     printf("Maybe soon I will learn new stuff about C programming and can apply it to making something cool!\n");
-    return 0; // Use of return 0 indicates sucessful execution
+    return 0; // Use of return 0 indicates successful execution
 }

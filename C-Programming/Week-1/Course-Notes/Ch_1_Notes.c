f79b66ff78ed3f3306877b37d5876c382ae956a8
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-03 00:34:33 -0400

    Update Ch_1_Notes.c

--- C-Programming/Week-1/Course-Notes/Ch_1_Notes.c
+++ C-Programming/Week-1/Course-Notes/Ch_1_Notes.c
@@ -6,21 +6,21 @@
 
  // Purpose of C Programming...?
     
-   // - C provides access to machine-level concepts that other langauges want to hide (using pointers and bytes)
-   // - C is usally used for applications that prioritize embedded systems and game development
+   // - C provides access to machine-level concepts that other languages want to hide (using pointers and bytes)
+   // - C is usually used for applications that prioritize embedded systems and game development
 
  // Origins of C Programming
     
-   // - C is considered a by-prject of UNIX
+   // - C is considered a by-project of UNIX
       
       // - Developed at Bell Laboratories
-      // - Major contributers Ken Thompson and Dennis Ritchie
+      // - Major contributions from Ken Thompson and Dennis Ritchie
 
  // What are the C-Based Languages?
     
     // C++: Super set of C with object-oriented programming
     // Objective - C: A superset of C with Object-oriented capabilities for Mac OS and iOS
     // Java: Based on C++ and therefore inherits many C features
-    // C#: A more recent langauge derived from C++ and Java concepts/fundamentals
+    // C#: A more recent language derived from C++ and Java concepts/fundamentals
 
 // PREPARE FOR THE FOLLOWING CHAPTERS OF INFORMATION AND FOLLOW ALONG!

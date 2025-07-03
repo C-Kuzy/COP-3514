0abb429529110dcb6325fae5600b8db5c9c0c72b
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-03 01:51:32 -0400

    C-Programming Notes Upload

---
+++ C-Programming/Week-1/Course-Notes/Ch_3_Notes.c
@@ -0,0 +1,14 @@
+/*
+ Author: C-Kuzy
+ Summer Semester Lecture #2
+ Description: Chapter 3 Formatted Input/Outputs
+*/
+
+ // What are Identifiers?
+
+    // Identifiers are variables, functions, macros, and other entities we use as they contain letters, digits, and underscores (cannot start with a digit)
+        // Examples:              times100          |          you_are_cool         |             power_of_3            |            _better
+
+        // It is common practice to avoid using identifiers that begin with an underscore to avoid confusion with other variables or functions
+        // Since C is case sensitive: There is a noticeable difference between lower and uppercase letters used in identifiers
+        // 

57d5981b9ce1988ab32004d64d5036ad2eba8a0d
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-05 23:35:58 -0400

    Update Ch_3_Notes.c

--- C-Programming/Week-1/Course-Notes/Ch_3_Notes.c
+++ C-Programming/Week-1/Course-Notes/Ch_3_Notes.c
@@ -1,6 +1,6 @@
 /*
  Author: C-Kuzy
- Summer Semester Lecture #2
+ Summer Semester Lecture #2 & #3
  Description: Chapter 3 Formatted Input/Outputs
 */
 
@@ -11,4 +11,15 @@
 
         // It is common practice to avoid using identifiers that begin with an underscore to avoid confusion with other variables or functions
         // Since C is case sensitive: There is a noticeable difference between lower and uppercase letters used in identifiers
-        // 
+        // Many programmers are mostly using lower-case letters for identifiers but I use upper-case letters to begin the identifier, it is good for personal preference
+
+    // Variables & Types 
+
+        // We need to be able to store data temporarily during the program's execution. These storage locations are known as variables
+            // They must have what's known as a "type". Which in C, there are a variety of types, some examples we deal with are int and double 
+                int // represents decimal integers as we can also use long int for large numbers... ex: 256
+                double // represents as a floating integer value that consists of decimals... ex: 219.225
+
+    // Printing Variable Values
+
+    

e520a27c0c842e0a806f257d953580a31580a51d
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-08 23:41:48 -0400

    Update Ch_4_Notes.c

--- C-Programming/Week-2/Course-Notes/Ch_4_Notes.c
+++ C-Programming/Week-2/Course-Notes/Ch_4_Notes.c
@@ -4,3 +4,18 @@
  Description: Chapter 4 
 */
 
+    // UNIX COMMAND BASICS
+
+        mv // moving a file or renaming
+        cp // coping a file
+        rm // remove a file
+            -i // this can prompt the file before removal
+        ctrl + l // clear the screen
+
+        // Basic Keyboard Tricks
+
+            //
+            //
+
+            //
+            //

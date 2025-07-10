840ae421e1d335e3a75730e009a517bddd763b72
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-09 23:59:31 -0400

    Update Ch_4_Notes.c

--- C-Programming/Week-2/Course-Notes/Ch_4_Notes.c
+++ C-Programming/Week-2/Course-Notes/Ch_4_Notes.c
@@ -11,6 +11,9 @@
         rm // remove a file
             -i // this can prompt the file before removal
         ctrl + l // clear the screen
+        tab // useful for easy completion within opening file libraries
+        up arrow key // Moves up to the previous entry 
+        down arrow key //
 
         // Basic Keyboard Tricks
 

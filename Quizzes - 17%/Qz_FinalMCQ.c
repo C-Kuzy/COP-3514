2fc8a081fb6176fbd632b7288c0d2d297649956b
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-21 23:32:14 -0400

    Update Qz_FinalMCQ.c

--- Quizzes - 17%/Qz_FinalMCQ.c
+++ Quizzes - 17%/Qz_FinalMCQ.c
@@ -1,4 +1,10 @@
 /*
- Author: Connor Kouznetsov
-
+ Author: C-Kuzy
+ Description: 2025 Summer C Semester: COP-3514 Final Exam Multiple Choice Answer Bank
 */
+
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <time.h>
+#define MX_Options 4

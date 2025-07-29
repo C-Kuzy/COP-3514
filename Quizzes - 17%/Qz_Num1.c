5cdd8a227323363c7a104eab31a19c3052f4ff09
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-29 18:19:07 -0400

    Update Qz_Num1.c

--- Quizzes - 17%/Qz_Num1.c
+++ Quizzes - 17%/Qz_Num1.c
@@ -18,15 +18,91 @@ struct {
     const char *why;                  // Got the Question wrong? There's feedback for you!
 } Question;
 
-/* REFERENCE QUESTIONS FOR Multiple Choice: Canvas -> COP 3514 -> Modules -> 1 - Pre-Test */
+/* REFERENCE QUESTIONS FOR Multiple Choice: Canvas -> COP 3514 -> Modules -> Week #2 -> 2 - Chapter 4 & 5 */
 static const Question quiz[] = {
 
     /* QUESTION #1 */
+    {"What is an lvalue in C programming?\n",
+        {"An error message",
+         "A label name",
+         "An object stored in computer memory",
+         "A literal constant"},
+        2,
+        ""},
 
     /* QUESTION #2 */
+    {"What does the statement 'count++' do?\n",
+        {"Decrements 'count' by 1",
+         "Increments 'count' by 1",
+         "Sets 'count' to 0",
+         "Multiplies 'count' by 2"},
+        1,
+        ""},
 
     /* QUESTION #3 */
+    {"Which of the following is a correct use of assignment chaining?\n",
+        {"i + j + k = 0;",
+         "i = j = k = 0;",
+         "i == j == k == 0;",
+         "i = j == k = 0;"},
+        1,
+        ""},
 
     /* QUESTION #4 */
+    {"What is the output of the following statement?\n\n",
+        {"32 5",
+         "32 4",
+         "40 5",
+         "40 4"},
+        }
 
-#define NQ ((int))
+    /* QUESTION #5 */
+
+
+    /* QUESTION #6 */
+
+
+    /* QUESTION #7 */
+
+
+    /* QUESTION #8 */
+
+
+    /* QUESTION #9 */
+
+
+    /* QUESTION #10 */
+
+
+    /* QUESTION #11 */
+
+
+    /* QUESTION #12 */
+
+
+    /* QUESTION #13 */
+
+
+    /* QUESTION #14 */
+
+
+    /* QUESTION #15 */
+
+
+    /* QUESTION #16 */
+
+
+    /* QUESTION #17 */
+
+
+    /* QUESTION #18 */
+
+
+    /* QUESTION #19 */
+
+
+    /* QUESTION #20 */
+    {{}}
+
+
+#define NQ ((int) )

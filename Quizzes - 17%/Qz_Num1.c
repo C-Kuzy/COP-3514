1690d6710237627c29af29c8cc17fee70dda7cb1
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-29 17:18:57 -0400

    Update Qz_Num1.c

--- Quizzes - 17%/Qz_Num1.c
+++ Quizzes - 17%/Qz_Num1.c
@@ -7,6 +7,7 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stddef.h>
 #define MX_Options 5
 
 /* ---------------------------- Question Bank Structure ---------------------------- */
@@ -16,3 +17,16 @@ struct {
     int correct;                      // Searches through 
     const char *why;                  // Got the Question wrong? There's feedback for you!
 } Question;
+
+/* REFERENCE QUESTIONS FOR Multiple Choice: Canvas -> COP 3514 -> Modules -> 1 - Pre-Test */
+static const Question quiz[] = {
+
+    /* QUESTION #1 */
+
+    /* QUESTION #2 */
+
+    /* QUESTION #3 */
+
+    /* QUESTION #4 */
+
+#define NQ ((int))

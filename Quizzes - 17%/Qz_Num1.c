a661db78b09788526edb2947042d76432348dbd6
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-07-30 11:09:03 -0400

    Update Qz_Num1.c

--- Quizzes - 17%/Qz_Num1.c
+++ Quizzes - 17%/Qz_Num1.c
@@ -24,37 +24,37 @@ static const Question quiz[] = {
     /* QUESTION #1 */
     {"What is an lvalue in C programming?\n",
         {"An error message",                     // 0 == A
-         "A label name",                         //
-         "An object stored in computer memory",  //
-         "A literal constant"},                  //  
-        2,
+         "A label name",                         // 1 == B
+         "An object stored in computer memory",  // 2 == C
+         "A literal constant"},                  // 3 == D
+        2,                                       // Answer = (2 == C)
         ""},
 
     /* QUESTION #2 */
     {"What does the statement 'count++' do?\n",
-        {"Decrements 'count' by 1",   //
-         "Increments 'count' by 1",   //
-         "Sets 'count' to 0",         //
-         "Multiplies 'count' by 2"},  //
-        1,                                       
+        {"Decrements 'count' by 1",   // 0 == A
+         "Increments 'count' by 1",   // 1 == B
+         "Sets 'count' to 0",         // 2 == C
+         "Multiplies 'count' by 2"},  // 3 == D
+        1,                            // Answer = (1 == B)  
         ""},
 
     /* QUESTION #3 */
     {"Which of the following is a correct use of assignment chaining?\n",
-        {"i + j + k = 0;",     //
-         "i = j = k = 0;",     //
-         "i == j == k == 0;",  //
-         "i = j == k = 0;"},   //
-        1,                     //
+        {"i + j + k = 0;",     // 0 == A
+         "i = j = k = 0;",     // 1 == B
+         "i == j == k == 0;",  // 2 == C
+         "i = j == k = 0;"},   // 3 == D
+        1,                     // Answer = (1 == B)
         ""},
 
     /* QUESTION #4 */
     {"What is the output of the following statement?\n\n",
-        {"32 5",   //
-         "32 4",   //
-         "40 5",   //
-         "40 4"},  //
-        0,         //
+        {"32 5",   // 0 == A
+         "32 4",   // 1 == B
+         "40 5",   // 2 == C
+         "40 4"},  // 3 == D
+        0,         // Answer = (0 == A)
         ""},
 
     /* QUESTION #5 */

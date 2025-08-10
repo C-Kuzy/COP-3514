1582f4b50f1de7e278913c4522619b035153963a
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-08-10 13:34:40 -0400

    Update Qz_Num1.c

--- Quizzes - 17%/Qz_Num1.c
+++ Quizzes - 17%/Qz_Num1.c
@@ -204,6 +204,17 @@ static const Question quiz[] = {
 
 #define NQ ((int)(sizeof quiz / sizeof quiz[0]))
 
+/* ---------- helpers ---------- */
+static void shuffle_int_array(int *a, int n) {
+    for (int i = n - 1; i > 0; --i)
+    {
+        int j = rand() % (i + 1);
+        int tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+}
+
 /* ---------- Main Function ---------- */
 int main(void) {
     srand((unsigned)time(NULL)); /* new order each run */

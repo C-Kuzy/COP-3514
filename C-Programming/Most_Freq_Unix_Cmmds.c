55e1bdb26161443dd9ff098849b123f80f4d4376
Connor Kouznetsov <161536397+C-Kuzy@users.noreply.github.com>
2025-08-21 23:16:12 -0400

    Update Most_Freq_Unix_Cmmds.c

--- C-Programming/Most_Freq_Unix_Cmmds.c
+++ C-Programming/Most_Freq_Unix_Cmmds.c
@@ -1,7 +1,24 @@
 /*
  Author: C-Kuzy
  Description: Command bank of the Most Frequent Unix Commands
- Course: COP 3514 // Program Design
+ Course: COP 3514 --- Program Design
 */
 
-// Currently Editing for future development of this course
+// Currently Editing 
+
+mkdir  ; // List the contents of the currently directory
+
+
+cd     ; // Create (make) a directory
+
+exit   ; // Change directory to the directory
+
+chmod  ; // Change the mode (permissions) of file or directory
+
+mv file1 file2   ;
+
+cp file1 file2   ;
+
+rm file(s)       ;
+
+/* KEYWORD TRICKS: */
